57975e19755d129827eb7756346a44d4d3710e3c
Mark Huo <huomian@outlook.com>
2017-02-04 21:13:40 +0800

    move component to Router.purs

--- chatty/src/Chatty/Router.purs
+++ chatty/src/Chatty/Router.purs
@@ -3,12 +3,12 @@ module Chatty.Router where
 import Prelude
 
 import Control.Alt ((<|>))
-import Control.Monad.State (State)
 
 import Data.Maybe (Maybe(..))
 
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 
 import Routing.Match (Match)
 import Routing.Match.Class (lit)
@@ -66,3 +66,41 @@ routing  =
 --     eval (ChangeRoute next) = do
 --         H.modify \st -> { currentPage: "" }
 --         pure next
+
+data Query a = ToggleState a
+
+type State = { on :: Boolean }
+
+component :: forall g. H.Component HH.HTML Query Unit Void g
+component =
+  H.component
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+    initialState :: State
+    initialState = { on: false }
+
+    render :: State -> H.ComponentHTML Query
+    render state =
+      HH.div_
+      [ HH.h1_
+        [ HH.text "Hello world!" ]
+      , HH.p_
+        [ HH.text "Why not toggle this button:" ]
+      , HH.button
+        [ HE.onClick (HE.input_ ToggleState) ]
+        [ HH.text
+          if not state.on
+          then "Don't push me"
+          else "I said don't push me!"
+        ]
+      ]
+
+    eval :: Query ~> H.ComponentDSL State Query Void g
+    eval (ToggleState next) = do
+      H.modify (\state -> { on: not state.on })
+      pure next
