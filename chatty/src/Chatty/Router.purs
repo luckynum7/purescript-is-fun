495b90cf855b67282246926461ab2f5a2141ddf7
Mark Huo <huomian@outlook.com>
2017-02-06 11:17:33 +0800

    Hello world in Router.purs

--- chatty/src/Chatty/Router.purs
+++ chatty/src/Chatty/Router.purs
@@ -3,12 +3,10 @@ module Chatty.Router where
 import Prelude
 
 import Control.Alt ((<|>))
-
 import Data.Maybe (Maybe(..))
 
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 
 import Routing.Match (Match)
 import Routing.Match.Class (lit)
@@ -17,9 +15,6 @@ data Locations
   = Dashboard -- "Home"
   | Chat
   | Profile
-
-data RouteQuery a
-  = ChangeRoute a
 
 oneSlash :: Match Unit
 oneSlash = lit "/"
@@ -45,33 +40,13 @@ routing  =
   profile <|>
   dashboard
 
--- component :: forall m. H.Component HH.HTML RouteQuery Unit Void m
--- component =
---   H.component
---     { initialState: const initialState
---     , render
---     , eval
---     , receiver: const Nothing
---     }
---   where
 
---     initialState :: State
---     initialState = { currentPage: "" }
+data RouteQuery a
+  = ChangeRoute a
 
---     render :: State -> H.ComponentHTML RouteQuery
---     render state =
---       HH.div_ [ HH.p_ [ HH.text "Hello world!" ] ]
+type State = { currentPage :: String }
 
---     eval :: RouteQuery ~> H.ComponentDSL State RouteQuery Void m
---     eval (ChangeRoute next) = do
---         H.modify \st -> { currentPage: "" }
---         pure next
-
-data Query a = ToggleState a
-
-type State = { on :: Boolean }
-
-component :: forall g. H.Component HH.HTML Query Unit Void g
+component :: forall m. H.Component HH.HTML RouteQuery Unit Void m
 component =
   H.component
     { initialState: const initialState
@@ -82,25 +57,51 @@ component =
   where
 
     initialState :: State
-    initialState = { on: false }
+    initialState = { currentPage: "" }
 
-    render :: State -> H.ComponentHTML Query
+    render :: State -> H.ComponentHTML RouteQuery
     render state =
-      HH.div_
-      [ HH.h1_
-        [ HH.text "Hello world!" ]
-      , HH.p_
-        [ HH.text "Why not toggle this button:" ]
-      , HH.button
-        [ HE.onClick (HE.input_ ToggleState) ]
-        [ HH.text
-          if not state.on
-          then "Don't push me"
-          else "I said don't push me!"
-        ]
-      ]
+      HH.div_ [ HH.p_ [ HH.text "Hello world!" ] ]
 
-    eval :: Query ~> H.ComponentDSL State Query Void g
-    eval (ToggleState next) = do
-      H.modify (\state -> { on: not state.on })
-      pure next
+    eval :: RouteQuery ~> H.ComponentDSL State RouteQuery Void m
+    eval (ChangeRoute next) = do
+        H.modify \st -> { currentPage: "" }
+        pure next
+
+-- data Query a = ToggleState a
+
+-- type State = { on :: Boolean }
+
+-- component :: forall g. H.Component HH.HTML Query Unit Void g
+-- component =
+--   H.component
+--     { initialState: const initialState
+--     , render
+--     , eval
+--     , receiver: const Nothing
+--     }
+--   where
+
+--     initialState :: State
+--     initialState = { on: false }
+
+--     render :: State -> H.ComponentHTML Query
+--     render state =
+--       HH.div_
+--       [ HH.h1_
+--         [ HH.text "Hello world!" ]
+--       , HH.p_
+--         [ HH.text "Why not toggle this button:" ]
+--       , HH.button
+--         [ HE.onClick (HE.input_ ToggleState) ]
+--         [ HH.text
+--           if not state.on
+--           then "Don't push me"
+--           else "I said don't push me!"
+--         ]
+--       ]
+
+--     eval :: Query ~> H.ComponentDSL State Query Void g
+--     eval (ToggleState next) = do
+--       H.modify (\state -> { on: not state.on })
+--       pure next
