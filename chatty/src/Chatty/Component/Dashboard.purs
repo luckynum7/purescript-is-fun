ae13216aec7f76da9a1744e938b1c8a926c4527e
Mark Huo <huomian@outlook.com>
2017-02-06 11:30:05 +0800

    prepare Views

--- chatty/src/Chatty/Component/Dashboard.purs
+++ chatty/src/Chatty/Component/Dashboard.purs
@@ -1,3 +1,35 @@
 module Chatty.Component.Dashboard where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
+
+import Halogen as H
+import Halogen.HTML as HH
+
+data Query a
+  = ChangeRoute a
+
+type State = { currentPage :: String }
+
+component :: forall m. H.Component HH.HTML Query Unit Void m
+component =
+  H.component
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+    initialState :: State
+    initialState = { currentPage: "" }
+
+    render :: State -> H.ComponentHTML Query
+    render state =
+      HH.div_ [ HH.p_ [ HH.text "Hello world!" ] ]
+
+    eval :: Query ~> H.ComponentDSL State Query Void m
+    eval (ChangeRoute next) = do
+        H.modify \st -> { currentPage: "" }
+        pure next
--- chatty/src/Chatty/Component/Profile.purs
+++ chatty/src/Chatty/Component/Profile.purs
@@ -1,3 +1,47 @@
 module Chatty.Component.Profile where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
+
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+
+data Query a = ToggleState a
+
+type State = { on :: Boolean }
+
+component :: forall g. H.Component HH.HTML Query Unit Void g
+component =
+  H.component
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+    initialState :: State
+    initialState = { on: false }
+
+    render :: State -> H.ComponentHTML Query
+    render state =
+      HH.div_
+      [ HH.h1_
+        [ HH.text "Hello world!" ]
+      , HH.p_
+        [ HH.text "Why not toggle this button:" ]
+      , HH.button
+        [ HE.onClick (HE.input_ ToggleState) ]
+        [ HH.text
+          if not state.on
+          then "Don't push me"
+          else "I said don't push me!"
+        ]
+      ]
+
+    eval :: Query ~> H.ComponentDSL State Query Void g
+    eval (ToggleState next) = do
+      H.modify (\state -> { on: not state.on })
+      pure next
